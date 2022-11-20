cf1ba4212756c8f4320f6776f4bb2cc92994300e
mononerv <me@mononerv.dev>
2022-11-20 12:26:02 +0100

    Fixes CMakeLists to use variables for platform specfic config

--- helloworld/src/main.cpp
+++ helloworld/src/main.cpp
@@ -1,7 +1,7 @@
 #include <cstdint>
 #include <iostream>
 
-auto main([[maybe_unused]]std::int32_t argc, [[maybe_unused]]char const* argv[]) -> std::int32_t {
+auto main([[maybe_unused]]int argc, [[maybe_unused]]char const* argv[]) -> int {
     std::cout << "Hello, World!\n";
     return 0;
 }
